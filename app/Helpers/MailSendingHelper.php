7b74e233f42e15191017aa5bca03924b012bdd60
babojamo <kevin.loquencio@gmail.com>
2020-08-11 20:35:32 +0800

    Add helper for single source of mail sending

---
+++ app/Helpers/MailSendingHelper.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Mail\Outsource\ApprovedMailable as OutsourceApproved;
+use App\Mail\Outsource\ApprovalMailable as OutsourceApproval;
+use App\Mail\Outsource\DeniedMailable as OutsourceDenied;
+
+use App\Mail\Request\ApprovedMailable as RequestApproved;
+use App\Mail\Request\ApprovalMailable as RequestApproval;
+use App\Mail\Request\DeniedMailable as RequestDenied;
+
+use App\Mail\Completions\ApprovedMailable as CompletionsApproved;
+use App\Mail\Completions\ApprovalMailable as CompletionsApproval;
+use App\Mail\Completions\DeniedMailable as CompletionsDenied;
+
+use Mail;
+use App\User;
+use App\ServiceRequest;
+
+class MailSendingHelper
+{
+    public static function sendOutsourceApproval(User $user,ServiceRequest $service_request)
+    {
+        Mail::to($user->email)->send(new OutsourceApproval($user,$service_request));
+    }
+
+    public static function sendOutsourceApproved(User $user,ServiceRequest $service_request)
+    {
+        Mail::to($user->email)->send(new OutsourceApproved($user,$service_request));
+    }
+
+    public static function sendOutsourceDenied(User $user,ServiceRequest $service_request)
+    {
+        Mail::to($user->email)->send(new OutsourceDenied($user,$service_request));
+    }
+
+
+
+    public static function sendRequestApproval(User $user,ServiceRequest $service_request)
+    {
+        Mail::to($user->email)->send(new RequestApproval($user,$service_request));
+    }
+
+    public static function sendRequestApproved(User $user,ServiceRequest $service_request)
+    {
+        Mail::to($user->email)->send(new RequestApproved($user,$service_request));
+    }
+
+    public static function sendRequestDenied(User $user,ServiceRequest $service_request)
+    {
+        Mail::to($user->email)->send(new RequestDenied($user,$service_request));
+    }
+
+
+
+    public static function sendCompletionsApproval(User $user,ServiceRequest $service_request)
+    {
+        Mail::to($user->email)->send(new CompletionsApproval($user,$service_request));
+    }
+
+    public static function sendCompletionsApproved(User $user,ServiceRequest $service_request)
+    {
+        Mail::to($user->email)->send(new CompletionsApproved($user,$service_request));
+    }
+
+    public static function sendCompletionsDenied(User $user,ServiceRequest $service_request)
+    {
+        Mail::to($user->email)->send(new CompletionsDenied($user,$service_request));
+    }
+
+
+}
